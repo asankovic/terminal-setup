cb68aa7ec79a13c5c2ac55e63e9695a34ceeb9e2
Alen Sanković <alen5.sankovic@gmail.com>
2024-05-01 17:48:31 +0200

    add linting

--- dotfiles/neovim/.config/nvim/lua/asankovic/plugins/lsp/mason.lua
+++ dotfiles/neovim/.config/nvim/lua/asankovic/plugins/lsp/mason.lua
@@ -1,53 +1,55 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "lua_ls",
-        "bashls",
-        "dockerls",
-        "eslint",
-        "jsonls",
-        -- "jqls",
-        "lemminx",
-        "yamlls",
-        "sqlls",
-        -- "graphql",
-        -- "pyright",
-        -- "gradle_ls
-        -- "groovyls",
-        -- "terraformls",
-        -- "ansiblels",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"lua_ls",
+				"bashls",
+				"dockerls",
+				"eslint",
+				"jsonls",
+				-- "jqls",
+				"lemminx",
+				"yamlls",
+				"sqlls",
+				-- "graphql",
+				-- "pyright",
+				-- "gradle_ls
+				-- "groovyls",
+				-- "terraformls",
+				-- "ansiblels",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "isort",    -- python formatter
-        "black",    -- python formatter
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+			},
+		})
+	end,
 }
