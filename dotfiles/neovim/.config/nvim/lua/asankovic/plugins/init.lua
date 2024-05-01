401e8298d7949127e8db51eaac1c713846e52593
Alen Sanković <alen5.sankovic@gmail.com>
2024-05-01 14:51:22 +0200

    add lazy git plugin

--- dotfiles/neovim/.config/nvim/lua/asankovic/plugins/init.lua
+++ dotfiles/neovim/.config/nvim/lua/asankovic/plugins/init.lua
@@ -86,5 +86,21 @@ return {
         {silent = true, noremap = true}
       )
     end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
   }
 }
